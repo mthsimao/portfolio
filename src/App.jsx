e4b7fb197dd5493a5deb8b44c5528e59aaac5dba
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2026-04-04 16:48:22 -0300

    update profile image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { ArrowRight, Sparkles } from "lucide-react";
-import imageMe from "./assets/images/me.jpeg";
+import imageMe from "./assets/images/me.png";
 import Header from "./components/Header";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
